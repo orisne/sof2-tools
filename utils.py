63c6f816f0197c92172eaff5a3e25fd87d26357a
Ori Sne <chuckapilhard@gmail.com>
2021-02-16 19:32:18 +0200

    Add files via upload
    fixed fill() function loop breaking if user set offsets to 0

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
-import os, re, math
+import re
 
 from Point import Point
 from Entity import Entity
 
 # Extracts and formats the entity code from a text 
-#* useful for getting the entities code from the binary files
+#* returns a collection of entities in string format
 def format_entities(string):
     regex = r'\{(\n\".*)*\"\n\}'
     test_str = string
@@ -17,6 +17,7 @@ def format_entities(string):
     return content
 
 # Takes entities in string form and returns a list of Entity objects
+#* returns a list of entities (python objects)
 def str_to_entities(string):
     test_str = format_entities(string)
     fullRange = r'\{.*?\}'
@@ -41,7 +42,7 @@ def str_to_entities(string):
     return entities_list
 
 # Filters unwanted Entities from a list of Entity objects, determined by classnames
-#* useful for filtering all the unnecessary entities in Binary Space Partitioning files
+#* returns a list of entities after filtration
 def filter_entities(ent_list):
     try:
         with open('ignore.list') as ignore:
@@ -54,28 +55,27 @@ def filter_entities(ent_list):
 
 # Fills a 3D rectangular box using two points objects as corners
 #* reuturns a list of point objects
-#^ offsets cannot be 0, the for loops will not run otherwise!
 def fill(point_1=Point(), point_2=Point(10,10,10), offset=Point(10,10,10), hollow=False):
+    
     points = []
     offset = Point(abs(offset.x), abs(offset.y), abs(offset.z))
     p1 = Point(min(point_1.x, point_2.x), min(point_1.y, point_2.y), min(point_1.z, point_2.z))
     p2 = Point(max(point_1.x, point_2.x), max(point_1.y, point_2.y), max(point_1.z, point_2.z))
-    border = [p1.x, p1.y, p1.z, p2.x, p2.y, p2.z]
     
-    if offset.x == 0 or offset.y == 0 or offset.z == 0:
-        print('offsets cannot be 0! setting to distance.')
+    # if user set offset to 0 sets it to the distance + 1 (to run atleast once in for loop)
+    offset.x = p2.x-p1.x+1 if offset.x == 0 else offset.x
+    offset.y = p2.y-p1.y+1 if offset.y == 0 else offset.y
+    offset.z = p2.z-p1.z+1 if offset.z == 0 else offset.z
     
-    if p1.x-p2.x == 0:
-        p2.x += 1
-    if p1.y-p2.y == 0:
-        p2.y += 1
-    if p1.z-p2.z == 0:
-        p2.z += 1
-        
-    offset.x = p2.x-p1.x if offset.x > p2.x-p1.x or offset.x == 0 else offset.x
-    offset.y = p2.y-p1.y if offset.y > p2.y-p1.y or offset.y == 0 else offset.y
-    offset.z = p2.z-p1.z if offset.z > p2.z-p1.z or offset.z == 0 else offset.z
+    # setting the border of the rectangle for Hollowing
+    border = [p1.x, 
+              p1.y, 
+              p1.z, 
+              int((p2.x-p1.x)), 
+              int((p2.y-p1.y)), 
+              int((p2.z-p1.z))]
     
+    # Loops through the axii and fill in the points in offset intervals
     for i in range(p1.z, p2.z+1, offset.z):
         for j in range(p1.y, p2.y+1, offset.y):
             for k in range(p1.x, p2.x+1, offset.x):
@@ -88,4 +88,5 @@ def fill(point_1=Point(), point_2=Point(10,10,10), offset=Point(10,10,10), hollo
                     points.append(Point(k,j,i))
 
     return points
+
 
